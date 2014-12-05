44a7bbc578387b168df8430c23bc6a7ee42f20cd
Nick Clemens <n.clem.mkt@gmail.com>
2014-12-05 01:04:04 +0000

    Store

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,14 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10, too_short: 'is too short, at least 10'}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
 end
