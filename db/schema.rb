67d04f8e7d445ec8a0f794cfbb57f486af7e8a07
Nick Clemens <n.clem.mkt@gmail.com>
2014-12-07 15:06:48 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203235603) do
+ActiveRecord::Schema.define(version: 20141207021419) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
